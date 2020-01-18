5ae7d21fee482bb5ed8600793dab09aac4c5420b
mgiatti <mb.giatti@gmail.com>
2020-01-18 20:34:39 -0300

    Add mobile application
    - Add maps api
    - Add webview page

---
+++ src/routes.js
@@ -0,0 +1,26 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Main from './pages/Main';
+import Profile from './pages/Profile';
+
+const Routes = createAppContainer(
+    createStackNavigator({
+        Main: {
+            screen: Main,
+            navigationOptions: {
+                title: 'DevRadar'
+            }
+        },
+        Profile
+    }, {
+        defaultNavigationOptions: {
+            headerTintColor: '#FFF',
+            headerStyle: {
+                backgroundColor: '#7D40E7',
+            }
+        }
+    })
+);
+
+export default Routes;
